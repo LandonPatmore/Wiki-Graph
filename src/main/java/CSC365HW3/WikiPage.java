02c66db4315a54d64eb734eb343e978dc86ce1d7
Landon Patmore <landon.patmore@gmail.com>
2017-04-29 23:37:18 -0400

    created the skeleton for the wikipage class

--- src/main/java/CSC365HW3/WikiPage.java
+++ src/main/java/CSC365HW3/WikiPage.java
@@ -4,4 +4,32 @@ package CSC365HW3;
  * Created by landon on 4/29/17.
  */
 public class WikiPage {
+    private String URL;
+    private String title;
+    private String words;
+    private WikiPage[] children;
+
+
+    public WikiPage(String u, String t, String w, WikiPage[] c){
+        this.URL = u;
+        this.title = t;
+        this.words = w;
+        this.children = c;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getWords() {
+        return words;
+    }
+
+    public WikiPage[] getChildren() {
+        return children;
+    }
 }
